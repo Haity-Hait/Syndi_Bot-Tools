91e6cc1350ea49553568676596653b31c83e21b8
Haity Hait <haityhait47@gmail.com>
2023-02-06 18:04:09 +0100

    Day 3

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -21,10 +21,50 @@ window.addEventListener('scroll', () => {
 window.addEventListener('scroll', () => {
     let content = document.querySelector('.add3');
     let contentPosition = content.getBoundingClientRect().top;
-    let screenPosition = window.innerHeight ;
+    let screenPosition = window.innerHeight;
     if(contentPosition < screenPosition){
         content.classList.add('active')
     }else{
         content.classList.remove('active')
+    }
+})
+window.addEventListener('scroll', () => {
+    let content = document.querySelector('.add4');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if(contentPosition < screenPosition){
+        content.classList.add('active')
+    }else{
+        content.classList.remove('active')
+    }
+})
+window.addEventListener('scroll', () => {
+    let content = document.querySelector('.add5');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if(contentPosition < screenPosition){
+        content.classList.add('active')
+    }else{
+        content.classList.remove('active')
+    }
+})
+window.addEventListener('scroll', () => {
+    let content = document.querySelector('.trans');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if(contentPosition < screenPosition){
+        content.classList.add('trans_active')
+    }else{
+        content.classList.remove('trans_active')
+    }
+})
+window.addEventListener('scroll', () => {
+    let content = document.querySelector('.trans1');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if(contentPosition < screenPosition){
+        content.classList.add('trans_active')
+    }else{
+        content.classList.remove('trans_active')
     }
 })
